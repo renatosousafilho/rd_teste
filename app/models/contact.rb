0e1d1d36400b84b7c68fd980fe64aa9559fecbd6
Renato Filho <renatosousafilho@gmail.com>
2017-01-03 23:35:45 -0300

    associate user to contacts and custom fields and contact with fields

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,4 +2,8 @@ class Contact < ActiveRecord::Base
   belongs_to :user
 
   validates_presence_of :email, :user
+
+  has_many :contact_fields
+
+  accepts_nested_attributes_for :contact_fields
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+
+  has_many :contacts
+  has_many :custom_fields
 end
