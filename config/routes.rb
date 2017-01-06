29716fafed68e5eb119a7673a1af51fba0a60f5f
Renato Filho <renatosousafilho@gmail.com>
2017-01-05 22:49:16 -0300

    setting to deploy in heroku

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :custom_fields
   resources :contacts
 
+  root :to => "contacts#index"
+
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
