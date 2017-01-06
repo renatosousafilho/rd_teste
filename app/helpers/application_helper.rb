848eab20fdd434f4e47bffff5e1ab7ab4910680f
Renato Filho <renatosousafilho@gmail.com>
2017-01-06 16:44:03 -0300

    refactoring render of custom field to a helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,16 @@ module ApplicationHelper
       :select
     end
   end
+
+
+  def input_for_field(form, field)
+    unless field.nil?
+      if field.kind == "combobox"
+        form.input :value, as: :select, collection: field.choices.split(","), label: field.name
+      else
+        form.input :value, as: input_for_kind(field.kind), label: field.name
+      end
+    end
+
+  end
 end
