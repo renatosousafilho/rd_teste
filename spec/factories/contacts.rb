76545e51cb127a3577fd49d03d2c230d44cf8400
Renato Filho <renatosousafilho@gmail.com>
2017-01-06 15:43:26 -0300

    refactor factory of contact to support values from nested fields

--- spec/factories/contacts.rb
+++ spec/factories/contacts.rb
@@ -2,5 +2,17 @@ FactoryGirl.define do
   factory :contact do
     name "John Doe"
     email "johndoe@mail.com"
+
+    factory :contact_with_text do
+      contact_fields [ContactField.new(value: '(86) 9999-0000')]
+    end
+
+    factory :contact_with_textarea do
+      contact_fields [ContactField.new(value: 'Lorem ipsum')]
+    end
+
+    factory :contact_with_combobox do
+      contact_fields [ContactField.new(value: 'masculine')]
+    end
   end
 end
--- spec/features/create_contact_spec.rb
+++ spec/features/create_contact_spec.rb
@@ -3,37 +3,39 @@ require 'rails_helper'
 
 describe 'Create a new contact' do
   let(:user) { create_logged_in_user }
-  let(:contact) { FactoryGirl.build(:contact) }
 
   scenario 'fill a form with text field' do
     FactoryGirl.create(:custom_field_text, user: user)
+    contact = FactoryGirl.build(:contact_with_text)
 
     visit "/contacts/new"
     fill_in 'Name', with: contact.name
     fill_in 'Email', with: contact.email
-    fill_in 'Phone', with: "(66)77778888"
+    fill_in 'Phone', with: contact.contact_fields.first.value
     click_button 'Save'
     expect(page).to have_content("Contato cadastrado com sucesso")
   end
 
   scenario 'fill a form with textarea field' do
     FactoryGirl.create(:custom_field_textarea, user: user)
+    contact = FactoryGirl.build(:contact_with_textarea)
 
     visit "/contacts/new"
     fill_in 'Name', with: contact.name
     fill_in 'Email', with: contact.email
-    fill_in 'Bio', with: "Lorem ipsum..."
+    fill_in 'Bio', with: contact.contact_fields.first.value
     click_button 'Save'
     expect(page).to have_content("Contato cadastrado com sucesso")
   end
 
   scenario 'fill a form with combobox field' do
     FactoryGirl.create(:custom_field_combobox, user: user)
+    contact = FactoryGirl.build(:contact_with_combobox)
 
     visit "/contacts/new"
     fill_in 'Name', with: contact.name
     fill_in 'Email', with: contact.email
-    select 'masculine', from: 'Gender'
+    select contact.contact_fields.first.value, from: 'Gender'
     click_button 'Save'
     expect(page).to have_content("Contato cadastrado com sucesso")
   end
