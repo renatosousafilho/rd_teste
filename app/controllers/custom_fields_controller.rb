0fca83cefa7c096c8ba4962c33ab987dac0c68ab
Renato Filho <renatosousafilho@gmail.com>
2017-01-06 16:00:06 -0300

    remove edit and update action in custom fields controller

--- app/controllers/custom_fields_controller.rb
+++ app/controllers/custom_fields_controller.rb
@@ -1,6 +1,5 @@
 class CustomFieldsController < ApplicationController
   before_filter :authenticate_user!
-  before_action :set_field, only: [:show, :edit, :update]
 
   def index
     @custom_fields = current_user.custom_fields.paginate(page: params[:page], per_page: 10)
@@ -21,22 +20,7 @@ class CustomFieldsController < ApplicationController
     end
   end
 
-  def edit
-  end
-
-  def update
-    if @custom_field.update(field_params)
-      redirect_to custom_fields_path, notice: "Campo atualizado com sucesso"
-    else
-      render :edit
-    end
-  end
-
   private
-
-  def set_field
-    @custom_field = current_user.custom_fields.find(params[:id])
-  end
 
   # TODO: set in before action parse to string if choices was a array
   def field_params
